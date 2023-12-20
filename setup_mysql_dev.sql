95536c6c6c199a4c2b829ffa988a3819870e5822
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-12-21 00:51:08 +0100

    added test database setup file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
--- prepares a MySQL server for the project
+-- prepares a MySQL server for the project development
 -- by creating a database and a user
 -- and granting the user all privileges on the database
 -- and flushing privileges
---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+-- prepares a MySQL server for the project testing
+-- by creating a database and a user
+-- and granting the user all privileges on the database
+-- and flushing privileges
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
