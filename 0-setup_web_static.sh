e1fcc7a25ae0b2e1cb810527233fe7badeecd39e
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2024-01-15 02:10:29 +0100

    fixed task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,7 +7,28 @@ sudo mkdir -p /data/web_static/shared/
 echo "Hello World!" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sed -i '/\/hbnb_static {/d' /etc/nginx/sites-available/default
-echo -e "\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n}" | sudo tee -a /etc/nginx/sites-available/default
-nginx -t
+printf %s "server {
+    listen 80;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html index.htm;
+    add_header X-Served-By $HOSTNAME;
+
+    location /hbnb_static {
+	alias /data/web_static/current;
+	index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+        root /etc/nginx/html;
+        internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+nginx -t 
 sudo service nginx restart
