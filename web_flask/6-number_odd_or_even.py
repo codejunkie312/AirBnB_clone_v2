82c311f6d664205cca90dc1244c6f560c07a86e9
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2024-01-28 21:02:30 +0100

    added dynamic route /number_odd_or_even

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """Function to print hello"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """Function to print HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C(text):
+    """Function to print C"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    """Function to print python"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Function to print number"""
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """Function to print number template"""
+    return render_template("5-number.html", number=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    """Function to print number is odd or even"""
+    if n % 2 == 0:
+        nature = "even"
+    else:
+        nature = "odd"
+    return render_template("6-number_odd_or_even.html",
+                           number=n, nature=nature)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
