df90d9858956fffd5039ef184e7fc212ad01cba6
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2024-01-28 14:40:05 +0100

    added strict_slashes option

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,11 +6,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def hello_HBNB():
     """Function to print hello"""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
