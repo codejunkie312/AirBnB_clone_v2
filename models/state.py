af21b21d272d6ccb413c6176a71e494d83a3a14d
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2024-02-01 03:58:49 +0100

    added

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, orm
 
@@ -7,16 +8,25 @@ from sqlalchemy import Column, String, orm
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = orm.relationship('City', backref='state', cascade='all, delete')
+    if models.storage_type == "db":
+        name = Column(String(128), nullable=False)
+        cities = orm.relationship('City', backref='state',
+                                  cascade='all, delete')
+    else:
+        name = ""
 
-    @property
-    def cities(self):
-        """ Getter for cities """
-        from models import storage
-        from models.city import City
-        cities_list = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                cities_list.append(city)
-        return cities_list
+    def __init__(self, *args, **kwargs):
+        """ Constructor """
+        super().__init__(*args, **kwargs)
+
+    if models.storage_type != "db":
+        @property
+        def cities(self):
+            """ Getter for cities """
+            from models import storage
+            from models.city import City
+            cities_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
